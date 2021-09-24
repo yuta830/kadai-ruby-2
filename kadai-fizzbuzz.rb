187c5d0d51dd45ad78194a5ff04a607564722748
yuta.kato <yyyutaa7@gmail.com>
2021-09-24 08:07:18 +0000

    kadai-ruby-2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -10,13 +10,5 @@ def fizzbuzz(num)
 end
 num_max = 100
 (1..num_max).each do |num|
-    if (num%15==0)
-        puts "FizzBuzz"
-    elsif (num%3==0)
-        puts "Fizz"
-    elsif (num%5==0)
-        puts "Buzz"
-    else
-        puts num
-    end
+    puts fizzbuzz(num)
 end
